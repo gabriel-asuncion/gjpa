f1e4f08fa4dce0318bcb254a01b754ae6be0fa84
gabriel-asuncion <gjpasuncion@gmail.com>
2021-12-28 23:03:52 +0800

    update dump
    hehe

---
+++ pages/gallery/gallery_script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("img").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+  
+  });
+$( document ).ready(function() {
+  var arr = [];
+while(arr.length < 20){
+    var r = Math.floor(Math.random() * 182) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+  for(let i = 1; i <= 20;i++){
+    var val = arr[i-1];
+    var url = 'img/'+val+'.jpg'; 
+    var image = new Image();
+    image.src = url;
+    var element = document.getElementById("gallery");
+    image.setAttribute("alt",val);
+    image.addEventListener("click", function test(){
+      var t = $(this).attr("src");
+      $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+      $("#myModal").modal();
+    });
+    element.appendChild(image);
+   }
+});
+
+
